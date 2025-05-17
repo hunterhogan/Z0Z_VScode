d41fc663e1150474a557ee16c9eb0d3f3f636348
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-05-17 14:33:39 -0500

    remove pyright: ignore (just uncheck type ignore in pylance).

--- extension.js
+++ extension.js
@@ -39,58 +39,7 @@ function activate(context) {
         );
     });
 
-    let addPyrightIgnore = vscode.commands.registerCommand('z0z_-extensions-for-visual-studio-code.addPyrightIgnore', async (problemItem) => {
-        // Extract diagnostic rule from the problemItem
-        if (!problemItem || !problemItem.code || !problemItem.code.value) {
-            vscode.window.showErrorMessage('Unable to determine diagnostic rule.');
-            return;
-        }
 
-        const diagnosticRule = problemItem.code.value;
-        
-        // Open the file with the issue
-        const resourceUri = vscode.Uri.parse(problemItem.resource.toString());
-        const document = await vscode.workspace.openTextDocument(resourceUri);
-        const editor = await vscode.window.showTextDocument(document);
-        
-        // Position is 0-indexed in VS Code API
-        const lineIndex = problemItem.startLineNumber - 1;
-        const line = document.lineAt(lineIndex);
-        const lineText = line.text;
-        
-        // Check if the line already has a pyright ignore comment
-        const pyrightIgnoreRegex = /# pyright: ignore\[(.*?)\]/;
-        const ignoreMatch = lineText.match(pyrightIgnoreRegex);
-        
-        let newText;
-        let position;
-
-        if (ignoreMatch) {
-            // Line already has an ignore comment, append to it
-            const existingRules = ignoreMatch[1];
-            const updatedRules = existingRules.includes(diagnosticRule) 
-                ? existingRules 
-                : `${existingRules}, ${diagnosticRule}`;
-                
-            const beforeComment = lineText.substring(0, ignoreMatch.index);
-            const afterComment = lineText.substring(ignoreMatch.index + ignoreMatch[0].length);
-            
-            newText = `${beforeComment}# pyright: ignore[${updatedRules}]${afterComment}`;
-            position = new vscode.Position(lineIndex, 0);
-        } else {
-            // Line doesn't have an ignore comment, add new one
-            newText = `${lineText} # pyright: ignore[${diagnosticRule}]`;
-            position = new vscode.Position(lineIndex, 0);
-        }
-        
-        // Create the edit
-        const edit = new vscode.WorkspaceEdit();
-        edit.replace(resourceUri, new vscode.Range(position, line.range.end), newText);
-        
-        // Apply the edit
-        await vscode.workspace.applyEdit(edit);
-        // vscode.window.showInformationMessage(`Added pyright ignore for "${diagnosticRule}"`);
-    });
 
     let hoverProvider = vscode.languages.registerHoverProvider('*', {
         provideHover(document, position, token) {
@@ -112,7 +61,6 @@ function activate(context) {
 
     context.subscriptions.push(disposable);
     context.subscriptions.push(toggleProblemsVisibility);
-    context.subscriptions.push(addPyrightIgnore);
     context.subscriptions.push(hoverProvider);
 }
 
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -2,9 +2,7 @@ const vscode = require('vscode');
 const assert = require('assert');
 
 suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Starting tests.');
-
-    test('Command registration', async () => {
+    vscode.window.showInformationMessage('Starting tests.');    test('Command registration', async () => {
         const commands = await vscode.commands.getCommands();
         assert.ok(commands.includes('z0z_-extensions-for-visual-studio-code.addToExclude'));
         assert.ok(commands.includes('z0z_-extensions-for-visual-studio-code.toggleProblemsVisibility'));
